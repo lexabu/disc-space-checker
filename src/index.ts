1ae6567ee4e4c717c8f8f2f63f91926574097736
JP Angelle <jp@angelle.me>
2022-01-30 22:35:48 -0600

    set up bot

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,55 @@
+import { NodeSSH } from 'node-ssh';
+import { Client, Intents } from 'discord.js';
 import { config } from 'dotenv';
+import * as cron from 'cron';
 
 config();
 
-console.log('hello world');
+const {
+  BIGTIME_IP_ADDRESS,
+  BIGTIME_PASSWORD,
+  BIGTIME_USERNAME,
+  BOOSH_DISCORD_USER_ID,
+  DISCORD_BOT_TOKEN,
+  JP_DISCORD_USER_ID,
+} = process.env;
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client.once('ready', async () => {
+  const scheduledMessage = new cron.CronJob(
+    '00 9 * * *',
+    async () => {
+      const ssh = new NodeSSH();
+
+      await ssh.connect({
+        host: BIGTIME_IP_ADDRESS,
+        username: BIGTIME_USERNAME,
+        password: BIGTIME_PASSWORD,
+      });
+
+      const { stdout } = await ssh.execCommand('df -h /dev/vda1');
+
+      const split = stdout.split(' ');
+
+      const percentUsage = split[split.length - 2];
+
+      const booshUserId = await client.users.fetch(
+        BOOSH_DISCORD_USER_ID as string,
+      );
+      const jpUserId = await client.users.fetch(JP_DISCORD_USER_ID as string);
+
+      booshUserId.send(`BigTime validator disc usage is at ${percentUsage}.`);
+      jpUserId.send(`BigTime validator disc usage is at ${percentUsage}.`);
+    },
+    null,
+    true,
+    'America/Chicago',
+  );
+
+  scheduledMessage.start();
+});
+
+client.login(DISCORD_BOT_TOKEN);
